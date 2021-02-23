c1e4b66552e9956438c5eac4112f0fbbd06f815c
Paul Gower <paul@lunamark.com>
2021-02-23 07:38:23 -0600

    Move default monkeys to monkey class

--- Ceramics/04-ApiCreationAndConsumptionApps/ApiCreationAndConsumptionApps/ApiCreationExercise/ApiCreationAndConsumptionApps.Models/Monkey.cs
+++ Ceramics/04-ApiCreationAndConsumptionApps/ApiCreationAndConsumptionApps/ApiCreationExercise/ApiCreationAndConsumptionApps.Models/Monkey.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ApiCreationAndConsumptionApps.Models
 {
@@ -7,5 +8,34 @@ namespace ApiCreationAndConsumptionApps.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+        
+        public static readonly List<Monkey> DefaultMonkeyList = new List<Monkey>()
+        {
+            new Monkey()
+            {
+                Name = "Edgar",
+                Description = "He has orange and black fur and a black tail",
+                // https://source.unsplash.com/250x250/?monkey
+                ImageUrl = "https://images.unsplash.com/photo-1610227369190-5f6b2ebfec59?w=250&h=250&crop=entropy&cs=tinysrgb&fit=crop&fm=jpg"
+            },
+            new Monkey()
+            {
+                Name = "Sam",
+                Description = "She has grey fur and a white tail",
+                ImageUrl = "https://images.unsplash.com/photo-1608825089053-f060747ecd3b?w=250&h=250&crop=entropy&cs=tinysrgb&fit=crop&fm=jpg"
+            },
+            new Monkey()
+            {
+                Name = "Johnny",
+                Description = "He has brown and white fur and a brown tail",
+                ImageUrl = "https://images.unsplash.com/photo-1611153854632-2422bbf0884b?w=250&h=250&crop=entropy&cs=tinysrgb&fit=crop&fm=jpg"
+            },
+            new Monkey()
+            {
+                Name = "Kevin",
+                Description = "He has orange and brown fur and a black tail",
+                ImageUrl = "https://images.unsplash.com/photo-1611539222172-0f7e76279dd5?w=250&h=250&crop=entropy&cs=tinysrgb&fit=crop&fm=jpg"
+            },
+        };
     }
 }
--- Ceramics/04-ApiCreationAndConsumptionApps/ApiCreationAndConsumptionApps/ApiCreationExercise/ApiCreationAndConsumptionApps.WebAPI/Controllers/MonkeysController.cs
+++ Ceramics/04-ApiCreationAndConsumptionApps/ApiCreationAndConsumptionApps/ApiCreationExercise/ApiCreationAndConsumptionApps.WebAPI/Controllers/MonkeysController.cs
@@ -12,36 +12,6 @@ namespace ApiCreationAndConsumptionApps.WebAPI.Controllers
     [Route("[controller]")]
     public class MonkeysController : ControllerBase
     {
-        private static readonly List<Monkey> Monkeys = new List<Monkey>()
-        {
-            new Monkey()
-            {
-                Name = "Edgar",
-                Description = "He has red fur and a white tail",
-                // https://source.unsplash.com/250x250/?monkey
-                ImageUrl = "https://via.placeholder.com/150"
-            },
-            new Monkey()
-            {
-                Name = "Sam",
-                Description = "She has black fur and a orange tail",
-                ImageUrl = "https://via.placeholder.com/150"
-            },
-            new Monkey()
-            {
-                Name = "Johnny",
-                Description = "He has orange fur and a black tail",
-                ImageUrl = "https://via.placeholder.com/150"
-            },
-            new Monkey()
-            {
-                Name = "Kevin",
-                Description = "He has brown fur and a black tail",
-                ImageUrl = "https://via.placeholder.com/150"
-            },
-            
-        };
-
         private readonly ILogger<MonkeysController> _logger;
 
         public MonkeysController(ILogger<MonkeysController> logger)
@@ -52,7 +22,7 @@ namespace ApiCreationAndConsumptionApps.WebAPI.Controllers
         [HttpGet]
         public IEnumerable<Monkey> Get()
         {
-            return Monkeys.ToArray();
+            return Monkey.DefaultMonkeyList.ToArray();
         }
     }
 }
